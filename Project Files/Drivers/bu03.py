5b3ce4e3eb73f9cdec9f32de465ca3640d3c7236
Ashi582 <50850434+Ashi582@users.noreply.github.com>
2026-03-01 13:33:17 +1100

    Update uwb documentation with tutorial

--- Project Files/Drivers/bu03.py
+++ Project Files/Drivers/bu03.py
@@ -1,6 +1,7 @@
 # this file contains base and tag configuration for the uwb BU03 boards using UART
 # list of AT commands https://core-electronics.com.au/attachments/uploads/bu03-at-commands.pdf
 # BU03 documentation https://core-electronics.com.au/attachments/uploads/CE10222_bu03-kit_v1.1.0_specification.pdf
+# Core Electronics Written and Video Guide https://core-electronics.com.au/guides/sensors/getting-started-with-ultra-wideband-and-measuring-distances-arduino-and-pico-guide/#configuring-the-bu03-boards
 # author: Ashika
 # last updated: 23/02/26
 
@@ -19,6 +20,7 @@ class BU03:
             print(msg)
 
     def configure(self, id, role, channel = 1, rate = 1): # ID, Role (0 = tag, 1 = base station), Channel, Rate
+        # uart.write('AT') can be used to test if AT commands are sending successfully, refer to docs for more info
         # send_at('AT+RESTORE') # factory reset
         self.send_at(f'AT+SETCFG={id},{role},{channel},{rate}') 
         self.send_at('AT+SAVE')
